274efde80aa77a68208797042ceeb7436c6d67ed
Matt Stone <matthew.stone12@gmail.com>
2022-01-10 13:18:27 -0800

    Deploy private MTx repo instead of public FH

--- packer/build_nextflow_crispr_screen.sh
+++ packer/build_nextflow_crispr_screen.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -e
+#
+# build-nextflow-crispr-screen.sh
+#
+# Install dependencies for nextflow-crispr-screen AMI
+#
+# Copyright (C) 2022 Matthew Stone <matthew.stone@modulustherapeutics.com>
 
 # Update packages
 sudo yum update -y
@@ -17,14 +23,7 @@ newgrp docker
 sudo systemctl start docker
 
 # Install AWS CLI
-# curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-# unzip awscliv2.zip
-# rm awscliv2.zip
-# sudo ./aws/install
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -f -p $HOME/miniconda
 $HOME/miniconda/bin/conda install -c conda-forge -y awscli
 rm Miniconda3-latest-Linux-x86_64.sh
-
-# Install crispr-screen pipeline
-git clone https://github.com/FredHutch/crispr-screen-nf.git
